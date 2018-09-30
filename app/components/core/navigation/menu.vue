cc43efeaae907c66e0167f1852db10cb351da268
DecentM <decentm@decentm.com>
2018-10-01 00:13:46 +0200

    refactor(components): Move navbar into its own component

--- app/components/core/navigation/menu.vue
+++ app/components/core/navigation/menu.vue
@@ -21,7 +21,7 @@ export default {
         {
           'route':   '/tools',
           'display': 'Tools',
-          'icon':    'toys',
+          'icon':    'build',
         },
         {
           'route':   '/settings',

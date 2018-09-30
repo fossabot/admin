38884abb8a733bed10a98bab921e4bddebfef5c2
DecentM <decentm@decentm.com>
2018-10-01 00:16:04 +0200

    feat: Use custom brand colours for Vuetify theme

--- app/plugins/vuetify.js
+++ app/plugins/vuetify.js
@@ -5,8 +5,8 @@ Vue.use(Vuetify, {'theme': {
   'primary':   '#005FFF',
   'accent':    '#FFB450',
   'secondary': '#3E28B0',
-  'info':      '#0D47A1',
-  'warning':   '#ffb300',
-  'error':     '#B71C1C',
-  'success':   '#2E7D32',
+  'info':      '#3273DC',
+  'warning':   '#F97A1F',
+  'error':     '#D80B00',
+  'success':   '#06BC5A',
 },})
